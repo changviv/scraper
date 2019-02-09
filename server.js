8a98fd5751d822dc496f0c147d54d2c7162e5071
Vivien <vchang103@gmail.com>
2019-02-09 09:14:35 -0800

    add heroku mongodb

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(routes)
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/craigslist", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/craigslist";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
 
 app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
